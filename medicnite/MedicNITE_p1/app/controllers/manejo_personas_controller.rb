abc202fd901ab44383edb85757bad1df7ef45e25
Esteban Gaete <zoraskiller@gmail.com>
2013-12-17 02:00:16 -0300

    Borrando todo porque el moshe tiene la cagá

--- medicnite/MedicNITE_p1/app/controllers/manejo_personas_controller.rb
+++
@@ -1,6 +0,0 @@
-class ManejoPersonasController < ApplicationController
-	before_filter :authenticate_usuario!
-	def index
-		
-	end
-end
--- medicnite/MedicNITE_p1/test/controllers/manejo_recintos_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ManejoRecintosControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
