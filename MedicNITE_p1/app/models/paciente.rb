0d3c5771840e177695f2cb33680b14560de02875
mosheman <moshehefra.landaeta@usach.cl>
2014-01-04 16:15:20 -0300

    Arreglé el problema de la db, no era el script sql, sino que había que frozar las pk en los modelos de la app. dbtoapp sigue viable, y es una buena opción para la integridad de los datos. Falta la redirección a una vista de tratamiento de RollBack de db.

--- MedicNITE_p1/app/models/paciente.rb
+++ MedicNITE_p1/app/models/paciente.rb
@@ -1,4 +1,6 @@
 class Paciente < ActiveRecord::Base
+	self.primary_key = 'idPersona'
+	self.primary_key = 'perIdPersona'
 	belongs_to	:paciente_estado
 	belongs_to	:paciente_tipo
 	#belongs_to	:persona
--- MedicNITE_p1/db/migrate/20131225183822_create_paciente_tipos.rb
+++
@@ -1,9 +0,0 @@
-class CreatePacienteTipos < ActiveRecord::Migration
-    def change
-	create_table "paciente_tipos", primary_key: "idTipoPaciente", force: true do |t|
-    		t.string "tipoPaciente", limit: 20
-
-      		t.timestamps
-    	end
-    end
-end
