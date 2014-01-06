40bf8a0ae61baec107250274eedce779bb549cf4
mosheman <moshehefra.landaeta@usach.cl>
2014-01-06 05:11:09 -0300

    conflicto en persona.rb y asignaciones/edit arreglados.

--- MedicNITE_p1/app/models/persona.rb
+++ MedicNITE_p1/app/models/persona.rb
@@ -1,12 +1,9 @@
 class Persona < ActiveRecord::Base
-<<<<<<< HEAD
+
  	has_one :paciente, :foreign_key => 'idPersona'
  	has_one :personal_rubro, :foreign_key => 'idPersona'
 
-	def self.search(search)
-=======
  	def self.search(search)
->>>>>>> 4936c25427d5f0f430d32a659e96a6619d52a842
 	  if search
 	    where('nombresPersona LIKE ?', "%#{search}%")
 	  else
